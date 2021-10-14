f9cdb6c6db0ada00d24f7d6ab4fcbeb01c26e96d
Minchonang <84889516+Minchonang@users.noreply.github.com>
2021-10-14 11:53:05 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+const API_KEY = "e622abf35a7624bec331543b89007a01";
+
+function onGeoOk(position) {
+	const lat = position.coords.latitude;
+	const lon = position.coords.longitude;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			const city = document.querySelector("#weather span:first-child");
+			const weather = document.querySelector("#weather span:last-child");
+
+			city.innerText = `${data.name},`;
+			weather.innerText = `현재 기온: ${Math.round(data.main.temp)}℃, 날씨: ${
+				data.weather[0].main
+			},`;
+		});
+}
+
+function onGeoErr() {
+	alert("Can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
